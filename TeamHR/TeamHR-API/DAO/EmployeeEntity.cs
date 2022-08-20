fc5277a88133dba7eb92b986f2ad9d2cbbba1fc4
Jimmy Mo <jimmymonz@gmail.com>
2022-08-21 04:24:26 +1200

    Create database and employee entity

---
+++ TeamHR/TeamHR-API/DAO/EmployeeEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace teamhr_api.DAO
+{
+    public class EmployeeEntity
+    {
+        [Key]
+        public Guid EmployeeId { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Location { get; set; }
+
+    }
+}
---
+++ TeamHR/TeamHR-API/Services/TeamContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using teamhr_api.DAO;
+
+namespace teamhr_api.Services
+{
+    public class TeamContext : DbContext
+    {
+        public DbSet<EmployeeEntity> Employees { get; set; }
+        public string DbPath { get; }
+
+        public TeamContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "teamhr.db");
+        }
+
+        //Configures EF to create a SQLite database file in the special "local" folder on your device.
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+}
