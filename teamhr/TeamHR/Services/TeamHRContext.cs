0e721c42dab0c2d1298a7d19cfd031938d3a4de3
Jimmy Mo <jimmymonz@gmail.com>
2022-08-20 16:51:24 +1200

    Established database

---
+++ teamhr/TeamHR/Services/TeamHRContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamHR.DAO;
+
+namespace TeamHR.Services
+{
+    public class TeamHRContext : DbContext
+    {
+        public DbSet<EmployeeEntity> Employees { get; set; }
+
+        public string DbPath { get; }
+        public TeamHRContext()
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "teamHR.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
